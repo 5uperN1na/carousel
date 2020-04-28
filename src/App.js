288def8719c2ff610e1aa00598f8e3e7ad1470e8
Paula Suarez <paula.suarez@freeman.com>
2020-04-28 18:41:22 -0500

    Adding updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Arms from './julien-arms.jpg';
+import Smiles from './vivien-smiles.jpg';
+import Dominoes from './julien-dominoes1.jpeg';
+import Fireplace from './vivien-fireplace.jpg';
+
+let styles = {
+
+	margin: 'auto',
+	width: '500px'
+
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+
+		<div style={styles}>
+			<Carousel>
+				<div>
+					<img src={Fireplace} />
+					<p className="legend">Posin'!</p>
+				</div>
+				<div>
+					<img src={Dominoes} />
+					<p className="legend">Dominoes!</p>
+				</div>
+				<div>
+					<img src={Smiles} />
+					<p className="legend">Vivien Smiles!</p>
+				</div>
+				<div>
+					<img src={Arms} />
+					<p className="legend">Julien Flys!</p>
+				</div>
+
+
+
+			</Carousel>
+		</div>
+	);
 }
 
 export default App;
